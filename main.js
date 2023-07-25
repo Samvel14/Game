b5b808b525e754bf0d28e6012eedb4eb59873bb7
Samvel14 <samvel.h37@tumo.org>
2023-07-25 17:50:22 +0400

    avelacrel em nor nkarner ev restarti hnaravorutyun

--- main.js
+++ main.js
@@ -1,85 +1,80 @@
 let correct;
-let seconds = 30;
+let seconds = 20;
 let correctAnswer = 0;
 let incorrectAnswer = 0;
-function gelElement(id) {
-    return document.getElementById(id)
-    function main() {
-        let options = [];
-        const maxOptions = 3;
-        while (options.length < maxOptions) {
-            let coun = getRandomCountry();
-            if (options.indexOf(coun) === -1) {
-                options.push(coun);
-            }
-
+function getElement(id) {
+    return document.getElementById(id);
+}
+function getRandomCountry() {
+    return countries[Math.round(Math.random() * (countries.length - 1))];
+}
+function main() {
+    let options = [];
+    const maxOptions = 3;
+    while (options.length < maxOptions) {
+        let coun = getRandomCountry();
+        if (options.indexOf(coun) === -1) {
+            options.push(coun);
         }
-        for (let i = 0; i < options.length; i++) {
-            getElementh(`optioin${i + 1}label`).innerHTIML = options[i].name;
-            getElementh(`optioin${i + 1}label`).value = options[i].name;
-            getElementh(`optioin${i + 1}label`).checked = false;
-        }
-        correct = options[Math.round(Math.random() * (options.length - 1))];
-        getElement("flag").src = correct.flag;
     }
-    function timer() {
-        setTimeout(finish, seconds * 1000);
-        gelElement("time").innerHTML = seconds;
-        let countdown = setInterval(function () {
-            seconds--;
-            gelElement("time").textContent = seconds;
-            if (seconds <= 0) clearInterval(function () {
-                secunds--;
-                getElements("timer").style.color = "#ff0000"
-            }, 1000);
+    for (let i = 0; i < options.length; i++) {
+        getElement(`option${i + 1}label`).innerHTML = options[i].name;
+        getElement(`option${i + 1}input`).value = options[i].name;
+        getElement(`option${i + 1}input`).checked = false;
+    }
+    correct = options[Math.round(Math.random() * (options.length - 1))];
+    getElement("flag").src = correct.flag;
+}
+function timer() {
+    setTimeout(finish, seconds * 1000);
+    getElement("time").innerHTML = seconds;
+    let countdown = setInterval(function () {
+        seconds--;
+        getElement("time").textContent = seconds;
+        if (seconds <= 0) clearInterval(countdown);
+        if (seconds === 5) getElement("time").style.color = "#ff0000";
+    }, 1000);
+}
+function check() {
+    let input;
+    try {
+        input = document.querySelector('input[name = "option"]:checked').value;
+    } catch {
+        return;
+    }
+    if (input === correct.name) {
+        correctAnswer++;
+        getElement("score").innerHTML = correctAnswer;
+    } else {
+        incorrectAnswer++;
+    }
+    main();
+}
+function finish() {
+    clearInterval(checkInterval);
+    getElement("alert").style.display = "block";
+    getElement("card").style.display = "none";
+    getElement("alertscore").innerHTML = correctAnswer;
+    let percentage = Math.round((correctAnswer / (correctAnswer + incorrectAnswer)) * 100);
+    if (isNaN(percentage)) {
+        resultForAnswers = 100;
+    } else {
+        if (percentage >= 75 && percentage < 95) {
+            resultForAnswers = "duq cucaberel eq lav ardyunq "
+        } else if (percentage >= 95) {
+            resultForAnswers = "duq cucaberel eq gerazanc ardyunq "
+        }
+        else if (percentage <75) {
+            resultForAnswers = "duq cucaberel eq vat ardyunq "
+        }
+    }
+    getElement("alertaccuracy").innerHTML = `${resultForAnswers}  `;
+}
 
-            function check() {
-                let input;
-                try {
+function refresh() {
+    location = location;
+}
 
-                    if (seconds === 8) {
-                        gelElement("time").style.color = "green";
-                    }
-                    if (seconds == 5) {
-                        gelElement("time").style.color = "red";
-                    }
-                    (1000);
-
-                    function getRandomCountry() {
-                        return conutiies[Math.floor(Math.random(countries.length - 1) * 10)]
-                    }
-
-                    function main() {
-                        coun = getRandomCountry();
-                        getElement("flag").src = coun.flag;
-                    }
-                    function check() {
-                        let input;
-                        try {
-                            input = document.querySelector('input[name="option"]:checked').value
-                        } catch {
-                            return
-                        }
-                        if (input == correct.name) {
-                            correctAnswer++;
-                            gelElement("score").innerHTML = correctAnswer;
-                        } else {
-                            incorrectAnswer++;
-                        }
-                        main();
-                    }
-                    function fihish(){
-                        clearInterval(checkInerval);
-                    
-                    }
-                    function finish() {
-                        clearInterval(checkInerval);
-                        let percentage = (correctAnswer / (correctAnswer + incorrectAnswer)) * 100;
-                        gelElement("alertaccuracy").innerHTML = ` qo ardyunqn  e ${percentage}%`;
-                    }
-                    let checkInerval = setInterval(check, 50);
-                    main();
-                    timer();
-
-
-                
+let checkInterval = setInterval(check, 50);
+main();
+timer();
---
+++ script.js
@@ -0,0 +1,27 @@
+const countries = [
+    {
+
+        "flag": "./images/Aluminum.png",
+        "name": "Aliuminyum"
+    }, {
+
+        "flag": "./images/Borium.jbg.jpeg",
+        "name": "Borium"
+    }, {
+
+        "flag": "./images/Calcium.jpg",
+        "name": "Calcium"
+    }, {
+
+        "flag": "./images/Kalium.png",
+        "name": "Kalium"
+    }, {
+        "flag": "./images/Magneyzium.jpg",
+        "name": "Magneyzium"
+    },
+    {
+        "flag": "./images/Natrium.jpg",
+        "name": "Natrium"
+    },
+]
+
